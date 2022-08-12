0b2917161584e75a3b9c9a91b3a6ac22860ab90e
viwichi_ <viwicodes@gmail.com>
2022-08-12 18:58:30 +0530

    slider and authors images updated

---
+++ src/Pages/GalleryPage.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import Hero from '../Components/Gallery/Hero'
+import Footer from '../Components/Common/Footer'
+import Nav from '../Components/Common/Nav'
+import Gallery from '../Components/Gallery/Gallery'
+
+const GalleryPage = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [])
+    return (
+        <div>
+            <Nav />
+            <Hero />
+            <Gallery />
+            <Footer />
+        </div>
+    )
+}
+
+export default GalleryPage

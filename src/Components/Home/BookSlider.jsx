0afb2d8c916e511bd1fcc57f1813fd96593897ed
viwichi_ <viwicodes@gmail.com>
2022-08-14 20:00:36 +0530

    convert slider to prop

--- src/Components/Home/BookSlider.jsx
+++ src/Components/Home/BookSlider.jsx
@@ -1,131 +1,24 @@
+
 import React, { useLayoutEffect } from 'react'
 import "./dist/style.css"
 import { useEffect, useState } from 'react'
+import BookCell from './SliderOne'
 const BookSlider = () => {
   return (
     <>
       <div style={{ marginTop: "5rem" }} className="book-store">
         <div className="book-slide">
           <div className="book js-flickity" data-flickity-options='{ "wrapAround": true }'>
-            <div className="book-cell book-cell-light-color">
-              <div className="book-img">
-                <img src={require("./dist/img/1.jpg")} alt="" className="book-photo" />
-              </div>
-              <div className="book-content">
-                <div className="book-title">പൊതുവിജ്ഞാന വിജയമന്ത്രങ്ങൾ</div>
-                <div className="book-author">by Vipin Thomas</div>
-
-
-
-              </div>
-            </div>
-            <div className="book-cell book-cell-dark-color">
-              <div className="book-img">
-                <img src={require("./dist/img/2.jpg")} alt=""
-                  className="book-photo" />
-              </div>
-              <div className="book-content">
-                <div className="book-title">വധശിക്ഷയ്ക്കു വിധിക്കപ്പെട്ടവന്റെ അവസാന ദിനങ്ങൾ</div>
-                <div className="book-author">by Victor Hugo</div>
-
-              </div>
-            </div>
-            <div className="book-cell book-cell-light-color">
-              <div className="book-img">
-                <img src={require("./dist/img/3.jpg")} alt=""
-                  className="book-photo" />
-              </div>
-              <div className="book-content">
-                <div className="book-title">Pavlon And The Dog</div>
-                <div className="book-author">by Sanjeev Kalarikkathara</div>
-
-              </div>
-            </div>
-            <div className="book-cell book-cell-dark-color">
-              <div className="book-img">
-                <img src={require("./dist/img/4.jpg")} alt=""
-                  className="book-photo" />
-              </div>
-              <div className="book-content">
-                <div className="book-title">വിശ്വസാഹിത്യ പാഠങ്ങൾ</div>
-                <div className="book-author">by Dr.Munjinada Padmakumar</div>
-
-
-              </div>
-            </div>
-            <div className="book-cell book-cell-light-color">
-              <div className="book-img">
-                <img src={require("./dist/img/5.jpg")} alt=""
-                  className="book-photo" />
-              </div>
-              <div className="book-content">
-                <div className="book-title">ഗീതാഞ്ജലി</div>
-                <div className="book-author">by Rabindranath Tagore</div>
-
-
-
-              </div>
-            </div>
-            <div className="book-cell book-cell-dark-color">
-              <div className="book-img">
-                <img src={require("./dist/img/6.jpg")} alt=""
-                  className="book-photo" />
-              </div>
-              <div className="book-content">
-                <div className="book-title">ഇന്ത്യ ചരിത്രം കുട്ടികൾക്ക്</div>
-                <div className="book-author">by Dr.Munjinada Padmakumar</div>
-
-
-              </div>
-            </div>
-            <div className="book-cell book-cell-light-color">
-              <div className="book-img">
-                <img src={require("./dist/img/7.jpg")} alt=""
-                  className="book-photo" />
-              </div>
-              <div className="book-content">
-                <div className="book-title">Slvador Dali</div>
-                <div className="book-author">by Dr.Munjinada Padmakumar</div>
-
-
-              </div>
-            </div>
-            <div className="book-cell book-cell-dark-color">
-              <div className="book-img">
-                <img src={require("./dist/img/8.jpg")} alt=""
-                  className="book-photo" />
-              </div>
-              <div className="book-content">
-                <div className="book-title">Gabriel Garcia Marques</div>
-                <div className="book-author">by Dr.Munjinada Padmakumar</div>
-
-
-              </div>
-            </div>
-            <div className="book-cell book-cell-light-color">
-              <div className="book-img">
-                <img src={require("./dist/img/9.jpg")} alt=""
-                  className="book-photo" />
-              </div>
-              <div className="book-content">
-                <div className="book-title">മലയാള ഗദ്യചരിത്രം</div>
-                <div className="book-author">by Dr. Shyni Thomas</div>
-
-
-              </div>
-            </div>
-            <div className="book-cell book-cell-dark-color">
-              <div className="book-img">
-                <img src={require("./dist/img/10.jpg")} alt=""
-                  className="book-photo" />
-              </div>
-              <div className="book-content">
-                <div className="book-title">Grigori</div>
-                <div className="book-author">by Dr. Munjinad Padmakumar</div>
-
-
-              </div>
-            </div>
+            <BookCell type="dark" image={require('./dist/img/1.jpg')} title="പൊതുവിജ്ഞാന വിജയമന്ത്രങ്ങൾ" author="Vipin Thomas" />
+            <BookCell type="light" image={require('./dist/img/2.jpg')} title="വധശിക്ഷയ്ക്കു വിധിക്കപ്പെട്ടവന്റെ അവസാന ദിനങ്ങൾ" author="Victor Hugo" />
+            <BookCell type="dark" image={require('./dist/img/3.jpg')} title="Pavlon And The Dog" author="Sanjeev Kalarikkathara" />
+            <BookCell type="light" image={require('./dist/img/4.jpg')} title="വിശ്വസാഹിത്യ പാഠങ്ങൾ" author="Dr.Munjinada Padmakumar" />
+            <BookCell type="dark" image={require('./dist/img/5.jpg')} title="ഗീതാഞ്ജലി" author="" />
+            <BookCell type="light" image={require('./dist/img/6.jpg')} title="ഇന്ത്യ ചരിത്രം കുട്ടികൾക്ക്" author="Dr.Munjinada Padmakumar" />
+            <BookCell type="light" image={require('./dist/img/7.jpg')} title="Slvador Dali" author="Dr.Munjinada Padmakumar" />
+            <BookCell type="light" image={require('./dist/img/8.jpg')} title="Gabriel Garcia Marques" author="Dr.Munjinada Padmakumar" />
+            <BookCell type="light" image={require('./dist/img/9.jpg')} title="മലയാള ഗദ്യചരിത്രം" author="Dr. Shyni Thomas" />
+            <BookCell type="light" image={require('./dist/img/10.jpg')} title="Grigori" author="Dr. Munjinad Padmakumar" />
           </div>
         </div>
       </div>
--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
                         <img className="w-100" src="img/carousel-1.jpg" alt="" />
                         <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
                             <div className="p-3" style={{ maxWidth: "900px" }}>
-                                <img width="50%" src="img/mine/romanson.png" alt="" />
+                                {/* <img width="50%" src="img/mine/romanson.png" alt="" /> */}
                                 {/* <!-- <h5 className="text-white text-uppercase mb-3 animated slideInDown">Creative & Innovative</h5> --> */}
                                 <h1 className="display-1 text-white mb-md-4 animated zoomIn">Unwavering Excellence
                                     In Printing And Publishing</h1>

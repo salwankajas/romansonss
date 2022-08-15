64f88852587ae06fdb0f02f81b220d633c528a1c
salwankajas <salwankajas@gmail.com>
2022-08-15 13:25:06 +0530

    auotmated,see more

--- src/Components/Gallery/Hero.jsx
+++ src/Components/Gallery/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
     return (
         <div>
-            <div className="container-fluid bg-primary py-5 bg-header" style={{ marginBottom: "90px" }}>
+            <div className="container-fluid bg-primary py-5 bg-header" /*style={{ marginBottom: "90px" }}*/>
                 <div className="row py-5">
                     <div className="col-12 pt-lg-5 mt-lg-5 text-center">
                         <h1 className="display-4 text-white animated zoomIn">Gallery</h1>
